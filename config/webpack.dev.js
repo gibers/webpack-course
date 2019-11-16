af3b9940e92547f03f8e60651f1c00aa4224859f
gibers <chbaptis@hotmail.fr>
2019-11-16 23:45:01 +0100

    plantage depuis l'ajout du loader awesome-typescript et l'ajout du fichier .tsconfig.json

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,67 +4,91 @@ const HTMLWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   entry: {
-    main: ["./src/main.js"]
+    main: ["./src/main.js"],
+    ts: ["./src/index.ts"]
   },
   mode: "development",
   output: {
     filename: "[name]-bundle.js",
     path: path.resolve(__dirname, "../dist"),
-    publicPath: "/"
+    publicPath: "/",
   },
   devServer: {
     contentBase: "dist",
     overlay: true,
-    stats: {
-      colors: true
-    }
+    hot: true,
+    // stats: {
+    //   colors: true
+    // }
   },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
+    module: {
+        rules: [
           {
-            loader: "babel-loader"
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
+            test: /\.js$/,
+            use: [{
+              loader: "babel-loader"
+            }],
+            exclude: /node_modules/
           },
-          { loader: "css-loader" }
-        ]
-      },
-      {
-        test: /\.jpg$/,
-        use: [
           {
-            loader: "file-loader",
-            options: {
-              name: "images/[name].[ext]"
-            }
+            test: /\.ts$/,
+            use: [{
+              loader: "awesome-typescript-loader"
+            }],
+            exclude: /node_modules/
+          },
+          {
+            test: /\.css$/,
+            use: [{ loader: "style-loader" },{ loader: "css-loader" }]
+          },
+          {
+            test: /\.sass$/,
+            use: [
+              { loader: "style-loader" },
+              { loader: "css-loader" },
+              { loader: "sass-loader" },
+            ]
+          },
+          {
+            test: /\.styl$/,
+            use: [
+              { loader: "style-loader" },
+              { loader: "css-loader" },
+              { loader: "postcss-loader" },
+              { loader: "stylus-loader" },
+            ]
+          },
+          {
+            test: /\.less$/,
+            use: [
+              { loader: "style-loader" },
+              { loader: "css-loader" },
+              { loader: "less-loader" },
+            ]
+          },
+          {
+            test: /\.html$/,
+            use: [{
+              loader: "html-loader",
+              options: {
+                attrs: ["img:src"],
+              }
+            }]
+          },{
+            test: /\.(jpg|gif|png)$/,
+            use: [{
+              loader: "file-loader",
+              options: {
+                name: "bertrand/[name].[ext]"
+              }
+            }]
           }
         ]
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      }
-    ]
-  },
+    },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HTMLWebpackPlugin({
-      template: "./src/index.html"
-    })
+      new webpack.HotModuleReplacementPlugin(),
+      new HTMLWebpackPlugin({
+        template: "./src/index.html"
+      })
   ]
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-require("babel-runtime/regenerator")
-require("webpack-hot-middleware/client?reload=true")
-require("./main.css")
-require("./images/link.jpg")
-require("./index.html")
+require("babel-runtime/regenerator");
+require("webpack-hot-middleware/client?reload=true");
+require("./main.styl");
+require("./images/link.jpg");
+require("./index.html");
+
+// alert("bonjour les hommes et les femmes");
+
+
+var a = async (args) => {
+    const { a, b } = args
+    await console.log("bonjour les hommes et les femmes ",a, b)
+    console.log("Done")
+};
+
+a({a: 1, b:2})
+
+console.log('bonjour madame.');
+
 
